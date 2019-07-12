1a443664ffc549d320ae5ea70597ca9c7df795fe
Joel INTO <joel.balmer@intoglobal.com>
2019-07-12 17:28:37 +0100

    First commit

---
+++ OdeToFood.Data/RestaurantData.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        private List<Restaurant> restaurants;
+
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant()
+                {
+                    Id = 1,
+                    Name = "Joel Curry",
+                    Location = "Brighton",
+                    Cuisine = CuisineType.Indian
+                },
+                new Restaurant()
+                {
+                    Id = 2,
+                    Name = "Joel Pizza",
+                    Location = "Brighton",
+                    Cuisine = CuisineType.Italian
+                },
+                new Restaurant()
+                {
+                    Id = 3,
+                    Name = "Joel Burritos",
+                    Location = "Brighton",
+                    Cuisine = CuisineType.Mexican
+                },
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants
+                orderby r.Name
+                select r;
+        }
+    }
+}
